834c9c8dfb9a0339cd140a6eda9d49c3482f80ce
enesbabaoglu <enessbabaoglu@gmail.com>
2021-04-22 00:08:13 +0300

    Add efcore

---
+++ ServerApp/Repositories/Concrete/SociAllContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5WebApiExample.Repositories.Concrete
+{
+    public class SociAllContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
+
