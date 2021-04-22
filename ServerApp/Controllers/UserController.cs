1dd4c516bf5ecf514b4bffda20781c3c864d43d6
enesbabaoglu <enessbabaoglu@gmail.com>
2021-04-23 02:21:51 +0300

    Generate Jwt Token

--- ServerApp/Controllers/UserController.cs
+++ ServerApp/Controllers/UserController.cs
@@ -1,9 +1,14 @@
+using System.Text;
 using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using ServerApp.DTO;
 using ServerApp.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 
 namespace ServerApp.Controllers
 {
@@ -13,51 +18,76 @@ namespace ServerApp.Controllers
     {
         private UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _configuration;
 
-        public UserController(UserManager<User> userManager,SignInManager<User> signInManager)
+        public UserController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration configuration)
         {
+            _configuration = configuration;
             _userManager = userManager;
             _signInManager = signInManager;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(UserForRegisterDTO model){
+        public async Task<IActionResult> Register(UserForRegisterDTO model)
+        {
 
-            var user = new User{
+            var user = new User
+            {
                 UserName = model.UserName,
-                Email= model.Email,
+                Email = model.Email,
                 Name = model.Name,
                 Gender = model.Gender,
                 Created = DateTime.Now
             };
-            var result = await _userManager.CreateAsync(user,model.Password);
-            if(result.Succeeded){
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
                 return Accepted(201);
             }
             return BadRequest(result.Errors);
 
         }
         [HttpPost("login")]
-        public async Task<IActionResult> Login (LoginDTO model){
+        public async Task<IActionResult> Login(LoginDTO model)
+        {
 
             var user = await _userManager.FindByNameAsync(model.UserName);
 
-            if(user==null){
-               return BadRequest(new {message = "Username is incorrect"});
+            if (user == null)
+            {
+                return BadRequest(new { message = "Username is incorrect" });
             }
-            var result = await _signInManager.CheckPasswordSignInAsync(user,model.Password,false);
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
 
-            if(result.Succeeded){
+            if (result.Succeeded)
+            {
 
-                return Ok(new { 
-                    Token = "token",
-                    UserName = model.UserName    
+                return Ok(new
+                {
+                    Token = GenerateJwtToken(user),
                 });
 
             }
             return Unauthorized();
 
         }
-        
+
+        private string GenerateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key =Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Secrets").Value);
+
+            var tokenDescriptor = new SecurityTokenDescriptor{
+                Subject = new ClaimsIdentity(new Claim[]{
+                    new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                    new Claim(ClaimTypes.Name,user.UserName)
+                }),
+                Expires=DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256Signature) 
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
